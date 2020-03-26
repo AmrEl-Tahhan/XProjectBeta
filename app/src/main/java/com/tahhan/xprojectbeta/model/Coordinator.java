f528b25f525293a970c2ed089c5908f95e4a7966
Amr El-Tahhan <amr.tahhan@outlook.com>
2020-03-27 00:00:27 +0200

    // init commit

---
+++ app/src/main/java/com/tahhan/xprojectbeta/model/Coordinator.java
@@ -0,0 +1,4 @@
+package com.tahhan.xprojectbeta.model;
+
+public class Coordinator {
+}
---
+++ app/src/main/java/com/tahhan/xprojectbeta/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tahhan.xprojectbeta.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tahhan.xprojectbeta.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
