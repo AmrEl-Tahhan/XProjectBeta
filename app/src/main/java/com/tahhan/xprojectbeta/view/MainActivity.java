d46a3a1d090fa92b0713344bf5bf9342c278df8a
Amr El-Tahhan <amr.tahhan@outlook.com>
2020-03-31 03:40:51 +0200

    // adding Password Auth

--- app/src/main/java/com/tahhan/xprojectbeta/view/MainActivity.java
+++ app/src/main/java/com/tahhan/xprojectbeta/view/MainActivity.java
@@ -5,12 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.tahhan.xprojectbeta.R;
+import com.tahhan.xprojectbeta.view.ui.login.LoginFragment;
+import com.tahhan.xprojectbeta.view.ui.login.RegisterFragment;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String LOGIN_FRAGMENT_TAG = "loginFragmentTag";
+    public static final String REGISTER_FRAGMENT_TAG = "register_fragment_tag";
+    private LoginFragment loginFragment;
+    private RegisterFragment registerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginFragment = (LoginFragment) getSupportFragmentManager().findFragmentByTag(LOGIN_FRAGMENT_TAG);
+        registerFragment = (RegisterFragment) getSupportFragmentManager().findFragmentByTag(REGISTER_FRAGMENT_TAG);
+        if (loginFragment == null && registerFragment == null) {
+            loadLoginFragment();
+        } else if (registerFragment == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    loginFragment, LOGIN_FRAGMENT_TAG);
+        } else {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    registerFragment, REGISTER_FRAGMENT_TAG);
+        }
+    }
+
+    private void loadLoginFragment() {
+        loginFragment = new LoginFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack(null)
+                .replace(R.id.fragment_container, loginFragment, LOGIN_FRAGMENT_TAG)
+                .commit();
     }
 }
--- app/src/main/java/com/tahhan/xprojectbeta/view/ui/login/RegisterFragment.java
+++ app/src/main/java/com/tahhan/xprojectbeta/view/ui/login/RegisterFragment.java
@@ -1,31 +1,34 @@
 package com.tahhan.xprojectbeta.view.ui.login;
 
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.fragment.app.Fragment;
-
+import android.content.Context;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.tahhan.xprojectbeta.view.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.tahhan.xprojectbeta.R;
+
 
 public class RegisterFragment extends Fragment {
-
-    private LoginViewModel loginViewModel;
+    private static final String TAG = "RegisterFragment";
+    private FirebaseAuth mAuth;
+    Button registerBtn;
+    EditText mailET;
+    EditText passwordET;
+    EditText passwordConfirmET;
 
     @Nullable
     @Override
@@ -38,101 +41,61 @@ public class RegisterFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
-                .get(LoginViewModel.class);
-
-        final EditText usernameEditText = view.findViewById(R.id.username);
-        final EditText passwordEditText = view.findViewById(R.id.password);
-        final Button loginButton = view.findViewById(R.id.login);
-        final ProgressBar loadingProgressBar = view.findViewById(R.id.loading);
-
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
-            }
-        });
-
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-            }
-        });
-
-        TextWatcher afterTextChangedListener = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // ignore
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // ignore
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-            }
-        };
-        usernameEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(usernameEditText.getText().toString(),
-                            passwordEditText.getText().toString());
-                }
-                return false;
-            }
-        });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        // Initialize Firebase Auth
+        registerBtn = view.findViewById(R.id.signUpBtn);
+        mailET = view.findViewById(R.id.EmailET);
+        passwordET = view.findViewById(R.id.register_password);
+        passwordConfirmET = view.findViewById(R.id.passwordConfirm);
+        mAuth = FirebaseAuth.getInstance();
+        registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
+                Log.d(TAG, "onClick: attempting to register.");
+                //check for null valued EditText fields
+                if (!isEmpty(mailET.getText().toString())
+                        && !isEmpty(passwordET.getText().toString())
+                        && !isEmpty(passwordConfirmET.getText().toString())) {
+                        //check if passwords match
+                        if (passwordET.getText().toString() .equals(passwordConfirmET.getText().toString()))  {
+                            //Initiate registration task
+                            register(mailET.getText().toString(), passwordET.getText().toString());
+                        } else {
+                            Toast.makeText(getActivity(), "Passwords do not Match", Toast.LENGTH_SHORT).show();
+                        }
+
+                } else {
+                    Toast.makeText(getActivity(), "You must fill out all the fields", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        if (getContext() != null && getContext().getApplicationContext() != null) {
-            Toast.makeText(getContext().getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
-        }
+    private void register(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(getActivity(), "Authentication succssessful.", Toast.LENGTH_SHORT)
+                                    .show();
+                            //   FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
-    private void showLoginFailed(@StringRes Integer errorString) {
-        if (getContext() != null && getContext().getApplicationContext() != null) {
-            Toast.makeText(
-                    getContext().getApplicationContext(),
-                    errorString,
-                    Toast.LENGTH_LONG).show();
-        }
+    private boolean isEmpty(String string) {
+        return string.equals("");
     }
+
+
+
+
 }
